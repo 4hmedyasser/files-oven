3637c92ce242aed6b2a6cc31cc307d3fbc84fe86
AhmdSaleh <48695238+AhmdSaleh@users.noreply.github.com>
2019-04-09 16:52:32 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,102 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
 using System.IO;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
 
 namespace Test
 {
     class Program
-    {
+    {        
+        String txtFileName;
+        String xmlFileName;
+        char rowDelimiter;
+        char columDelimiter;
+        string Data;
+        string[] records;
+        List<List<String>> list;
+        int rowsSize;
+
+        Program(String txtFileName, String xmlFileName, char rowDelimiter, char columDelimiter) {
+
+            this.txtFileName = txtFileName;
+            this.xmlFileName = xmlFileName;
+            this.rowDelimiter = rowDelimiter;
+            this.columDelimiter = columDelimiter;
+
+        }
+
+        public void Null()
+        {
+
+            for (int i = 0; i < Data.Length - 1; i++)
+            {
+                if (Data[i] == columDelimiter && Data[i + 1] == columDelimiter)
+                    Data.Insert(i + 1, "Ǝ");
+            }
+        }
+
+
+        public void getText() {
+
+            Data = File.ReadAllText(txtFileName);
+            records = Data.Split(rowDelimiter);
+            list = new List<List<String>>();
+
+            foreach (string record in records)
+            {
+                string[] fields = record.Split(columDelimiter);
+                List<String> temp = new List<String>();
+
+                foreach (string field in fields)
+                {
+                    temp.Add(field);
+                }
+
+                list.Add(temp);
+            }
+
+            Null();
+
+            if (rowDelimiter == '\n')
+                rowsSize = list.ElementAt(0).Count-1 ;
+
+        }
+
+        public void setXML()
+        {
+
+            XmlWriter writer = XmlWriter.Create(xmlFileName);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Table");
+
+            for (int i = 1; i < rowsSize ; i++) {
+                writer.WriteStartElement("Row"+i);
+                for (int j = 0; j < list.ElementAt(0).Count; j++) {
+                    writer.WriteStartElement(list.ElementAt(0).ElementAt(j));
+                    writer.WriteString(list.ElementAt(i).ElementAt(j));
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+
+        }
+
+        
+        
         string[] cellDefaults(int no0fColumns)
         {
             string[] defaultValues = new string[no0fColumns];
